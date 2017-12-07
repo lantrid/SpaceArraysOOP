ae1a16236a88ca25cecb47279666bed199cf5500
Oleg Markov <33725311+lantrid@users.noreply.github.com>
2017-12-07 13:30:28 +0300

    Update Main3Activity.java

--- app/src/main/java/ru/samsung/itschool/spacearrays/Main3Activity.java
+++ app/src/main/java/ru/samsung/itschool/spacearrays/Main3Activity.java
@@ -1,9 +1,9 @@
 package ru.samsung.itschool.spacearrays;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class Main3Activity extends AppCompatActivity {
+public class Main3Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
