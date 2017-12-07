03981cef9011348ff39ee9bfaf11349358f71453
Oleg Markov <33725311+lantrid@users.noreply.github.com>
2017-12-07 12:47:05 +0300

    Update MainActivity.java

--- app/src/main/java/ru/samsung/itschool/spacearrays/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/spacearrays/MainActivity.java
@@ -1,13 +1,32 @@
 package ru.samsung.itschool.spacearrays;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    static int f = 0;
+    static int s = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void addRocket(View v) {
+        MyDraw.onAddRocket();
+        f++;
+        if(f>=10) showToast(v);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    public void addStar(View v) {
+        MyDraw.onAddStar();
+        s++;
+        if(s>=500) showToast(v);
+    }
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, "Максимальное количество ракет. Игра закончена.", Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
