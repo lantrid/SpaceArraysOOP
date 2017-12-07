12684d39f08a761262335c74f00c666fb99c3137
Oleg Markov <33725311+lantrid@users.noreply.github.com>
2017-12-07 12:51:44 +0300

    Add files via upload

---
+++ app/src/main/java/ru/samsung/itschool/spacearrays/Main2Activity.java
@@ -0,0 +1,27 @@
+package ru.samsung.itschool.spacearrays;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void startActivity(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Intent i = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(i);
+                break;
+        }
+    }
+}
+
