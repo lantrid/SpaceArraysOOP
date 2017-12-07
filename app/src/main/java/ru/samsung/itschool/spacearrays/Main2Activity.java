8558b91c1dc4e7a7e23207523e4d9514917b6de6
Oleg Markov <33725311+lantrid@users.noreply.github.com>
2017-12-07 13:30:59 +0300

    Update Main2Activity.java

--- app/src/main/java/ru/samsung/itschool/spacearrays/Main2Activity.java
+++ app/src/main/java/ru/samsung/itschool/spacearrays/Main2Activity.java
@@ -3,7 +3,6 @@ package ru.samsung.itschool.spacearrays;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
